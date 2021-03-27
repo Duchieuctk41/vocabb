5e805846b7ac527ad01aa37ecbcb5aa08a4e467b
duc hieu <duchieu.ctk41@gmail.com>
2021-03-27 20:30:53 +0700

    upload game pages

--- client/src/pages/game/game.component.jsx
+++ client/src/pages/game/game.component.jsx
@@ -1,23 +1,64 @@
 import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+
+import { close, cold, ramen, rice } from "../../img";
 
 // redux
 import { useDispatch, useSelector } from "react-redux";
 import { vocabActions } from "../../redux/actions/vocabActions";
 
+import style from "./game.module.scss";
+
 const Game = () => {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(vocabActions());
-  }, [dispatch]);
-
-  const { vocab } = useSelector((state) => state.vocab);
-  // console.log(vocab);
-
   return (
-    <div>
-      {vocab.map((item) => {
-        return <h2>{item.EnName}</h2>;
-      })}
+    <div className={style.container}>
+      <div className={style.header}>
+        <img src={close}></img>
+        <div className={style.process}></div>
+      </div>
+      <div className={style.content}>
+        <div className={style.grid}>
+          <h1>
+            <span>Đâu là "nóng"?</span>
+          </h1>
+          <div className={style.answer}>
+            {/* item */}
+            <div className={style.answer__item}>
+              <div className={style["answer__item-img"]}>
+                <img src={cold}></img>
+              </div>
+              <div className={style["answer__item-sub"]}>
+                <span>noodles</span>
+                <span>1</span>
+              </div>
+            </div>
+            <div className={style.answer__item}>
+              <div className={style["answer__item-img"]}>
+                <img src={rice}></img>
+              </div>
+              <div className={style["answer__item-sub"]}>
+                <span>noodles</span>
+                <span>1</span>
+              </div>
+            </div>
+            <div className={style.answer__item}>
+              <div className={style["answer__item-img"]}>
+                <img src={ramen}></img>
+              </div>
+              <div className={style["answer__item-sub"]}>
+                <span>noodles</span>
+                <span>1</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={style.footer}>
+        <div className={style.footer__container}>
+          <Link to="/">Bỏ qua</Link>
+          <Link to="/">Kiểm tra</Link>
+        </div>
+      </div>
     </div>
   );
 };
