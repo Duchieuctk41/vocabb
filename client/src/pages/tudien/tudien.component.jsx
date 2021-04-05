e53522aa00db6391e1b5bf3531411e8b1dfdeb8e
duc hieu <duchieu.ctk41@gmail.com>
2021-04-05 20:42:51 +0700

    effect_tudien

--- client/src/pages/tudien/tudien.component.jsx
+++ client/src/pages/tudien/tudien.component.jsx
@@ -1,26 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-import { usvi, cnvi, down, babyboy } 
-from "../../img";
+import { usvi, cnvi, down, babyboy } from "../../img";
 
 import style from "./tudien.module.scss";
 
 const Tudien = () => {
+  const [bg, setBg] = useState(false);
   return (
     <div className={style.main}>
-      <div className={style.header}></div>
       <div className={style.content}>
         <div className={style.center}>
-          <h1>Dịch Tiếng Anh - Tiếng Việt</h1>
+          <h1>Dịch Tiếng Anh-Tiếng Việt</h1>
           <h2>Xem giải nghĩa từ, ví dụ câu và nhiều điều khác.</h2>
-          <div className={style.translate}>
-          
+          <div
+            className={style.translate}
+            style={bg ? { background: "#fff" } : { background: "#f7f7f7" }}
+          >
             <Link className={style.img} to="#">
               <img src={usvi} alt="anh viet" name="language"></img>
               <img src={down} alt="down" name="down"></img>
             </Link>
-            <input type="text" placeholder="Nhập tiếng Anh hoặc tiếng Việt" />
+            <input
+              onFocus={() => {
+                setBg(true);
+              }}
+              onBlur={() => {
+                setBg(false);
+              }}
+              type="text"
+              placeholder="Nhập bằng Tiếng Anh hoặc Tiếng Việt"
+            />
             <Link name="translate" to="#">
               Dịch nghĩa
             </Link>
@@ -28,29 +38,44 @@ const Tudien = () => {
         </div>
       </div>
       <div className={style.botton}>
+        <hr className={style.hr}/>
         <ul className="menu">
           <li>
             <Link className="item" to="#">
               Giới thiệu
             </Link>
+          </li>
+          <li>
             <Link className="item" to="#">
               Học đường
             </Link>
+          </li>
+          <li>
             <Link className="item" to="#">
               Ứng dụng
             </Link>
+          </li>
+          <li>
             <Link className="item" to="#">
               Trợ giúp
             </Link>
+          </li>
+          <li>
             <Link className="item" to="#">
               Nội quy
             </Link>
+          </li>
+          <li>
             <Link className="item" to="#">
               Công việc
             </Link>
+          </li>
+          <li>
             <Link className="item" to="#">
               Điều khoản
             </Link>
+          </li>
+          <li>
             <Link className="item" to="#">
               Quyền riêng tư
             </Link>
