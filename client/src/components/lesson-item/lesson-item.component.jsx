dfeb7dc5cabcf1d12a7fc4b461f3ec872d5577b9
duc hieu <duchieu.ctk41@gmail.com>
2021-04-06 14:52:12 +0700

    fail

--- client/src/components/lesson-item/lesson-item.component.jsx
+++ client/src/components/lesson-item/lesson-item.component.jsx
@@ -8,22 +8,19 @@ import { chick, corona } from "../../img";
 import style from "./lesson-item.module.scss";
 
 const LessonItem = ({ lesson }) => {
-  const [toggle, setTogle] = useState(false);
+  const [toggle, setToggle] = useState(false);
   return (
-    <div className={style.main}>
-      <div className={style.itemne}>
-        {toggle ? <Popup /> : null}
-        <Link className={style.grid__item} onClick={() => setToggle(!toggle)}>
-          <div className={style["grid__item-img"]}>
-            {/* <span className={style.border}></span>
-            <div className={style["grid__item-img--purple"]}>
-              <img src={chick} alt="chick"></img>
-              <img src={corona} alt="corona" />
-            </div> */}
+    <div>
+      <Link className={style.grid__item}>
+        <div className={style["grid__item-img"]}>
+          <span className={style.border}></span>
+          <div className={style["grid__item-img--purple"]}>
+            <img src={chick} alt="chick"></img>
+            <img src={corona} alt="corona" />
           </div>
-          <h3>{lesson}</h3>
-        </Link>
-      </div>
+        </div>
+        <h3>{lesson}</h3>
+      </Link>
     </div>
   );
 };
