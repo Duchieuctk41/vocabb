eb6b96d95c434a6ce9a68f9c33cbe826441584e1
duc hieu <duchieu.ctk41@gmail.com>
2021-04-06 13:06:57 +0700

    revert code

--- client/src/components/lesson/lesson.component.jsx
+++ client/src/components/lesson/lesson.component.jsx
@@ -1,39 +1,26 @@
-import React, { useState } from "react";
+import React from "react";
 
 // import component
 import LessonItem from "../lesson-item/lesson-item.component";
 
-import Popup from "../popup/popup.component";
 // import img && scss
 import "./lesson.style.scss";
 
 const Lesson = ({ lesson, content }) => {
-  //toggle popup
-  // const [toggle, setToggle] = useState(false);
   // console.log(lesson);
   let item = [lesson.name, lesson.img, lesson._id];
   if (content !== undefined) {
     let item2 = [content.name, content.img, content._id];
-
     return (
       <div className="grid">
         <LessonItem lesson={item[0]} key={`${item[3]}-item`} />
         <LessonItem lesson={item2[0]} key={`${item2[2]}-item`} />
-        <Popup />
       </div>
     );
   } else {
     return (
       <div className="grid">
-        <LessonItem
-          lesson={item[0]}
-          key={`${item[2]}-item`}
-          // onClick={() => {
-          //   setToggle(!toggle);
-          // }}
-        />
-        {/* {toggle ? <Popup /> : null} */}
-        <Popup />
+        <LessonItem lesson={item[0]} key={`${item[2]}-item`} />
       </div>
     );
   }
