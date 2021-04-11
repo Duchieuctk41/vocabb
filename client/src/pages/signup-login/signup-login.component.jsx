6fca866d4cedc9d6eda92f77c90a49bab01832ae
duc hieu <duchieu.ctk41@gmail.com>
2021-04-11 09:11:34 +0700

    divide login-link component

--- client/src/pages/signup-login/signup-login.component.jsx
+++ client/src/pages/signup-login/signup-login.component.jsx
@@ -1,21 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import LoginLink from "./../../components/login-link/login-link.component";
 import SignUp from "../../components/signup/signup.component";
 import SignIn from "../../components/signin/signin.component";
 
-import { close } from "../../img";
 
 import style from "./signup-login.module.scss";
 
 const SignupLogin = () => (
   <div className={style.content}>
-    <Link to="/">
-      <img src={close} alt="cross" className={style.cross}></img>
-    </Link>
-    <Link to="/" className={style.signup}>
-      Đăng Ký
-    </Link>
+    <LoginLink />
     {/* <SignUp /> */}
     <SignIn />
   </div>
