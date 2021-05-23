c8794695f8ad8dbaba67ed97d91baef5aca9532d
duchieu <duchieu.ctk41@gmail.com>
2021-05-23 23:24:21 +0700

    chinh sua env.example

--- server/sh/env.example.sh
+++ server/sh/env.example.sh
@@ -1,23 +1,23 @@
 # Config database enviroment variable
-  export DB_CONNECTION=mongodb+srv
-  export DB_HOST=localhost
-  export DB_PORT=27017
-  export DB_NAME=vocab
-  export DB_USERNAME=duchieu
-  export DB_PASSWORD=thuyduong1112001
+  export DB_CONNECTION=***
+  export DB_HOST=***
+  export DB_PORT=***
+  export DB_NAME=***
+  export DB_USERNAME=***
+  export DB_PASSWORD=***
 
 # Config app enviroment variable
-  export APP_HOST=localhost
-  export APP_PORT=3001
+  export APP_HOST=***
+  export APP_PORT=***
 
 # Config admin email account
-  export MAIL_USER=duchieu.ctk41@gmail.com
-  export MAIL_PASSWORD=thuyduong1112001
-  export MAIL_HOST=smtp.gmail.com
-  export MAIL_PORT=587 
+  export MAIL_USER=***
+  export MAIL_PASSWORD=***
+  export MAIL_HOST=***
+  export MAIL_PORT=*** 
 
 # Config cloudinary
-  export CLOUDINARY_NAME=duchieu
-  export CLOUDINARY_KEY=894513215544578
-  export CLOUDINARY_SECRET=iL6nD0ldkOfBNOo7A_fOsLedSRE
-  export CLOUDINARY_URL=cloudinary://894513215544578:iL6nD0ldkOfBNOo7A_fOsLedSRE@duchieu
+  export CLOUDINARY_NAME=*** 
+  export CLOUDINARY_KEY=*** 
+  export CLOUDINARY_SECRET=*** 
+  export CLOUDINARY_URL=*** 
